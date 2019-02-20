8b68c3d0cfb9aa1131fb7e06d65ed94e45b855f9
jashpreet <jashpreetmalhotra@gmail.com>
2019-02-20 18:08:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/steptracker/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.hp.steptracker
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() , SensorEventListener {
+
+    val sensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+
+    var totalsteps=0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        stepCount.setText(totalsteps.toString())
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+       val stepCounter= sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+
+        sensorManager.registerListener(this , stepCounter ,SensorManager.SENSOR_DELAY_FASTEST)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let {
+
+//          var values =event.values
+//
+//            if(values[0]==1)
+            totalsteps= totalsteps+1
+            stepCount.setText(totalsteps.toString())
+
+        }
+    }
+}
